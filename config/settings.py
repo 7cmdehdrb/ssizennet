33adc451bf09a399dce69b2efbe70280d2216438
7cmdehdrb <7cmdehdrb@naver.com>
2020-03-16 18:41:59 +0900

    finalplz

--- config/settings.py
+++ config/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import sentry_sdk
+from django.conf import settings
 from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -27,10 +28,8 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = bool(os.environ.get("DEBUG"))
+DEBUG = bool(os.environ.get("DEBUG"))
 # DEBUG = True
-
-DEBUG = True
 
 
 ALLOWED_HOSTS = [".elasticbeanstalk.com", "127.0.0.1"]
@@ -160,15 +159,20 @@ MEDIA_URL = "/media/"
 
 # sentry
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-AWS_ACCESS_KEY_ID = os.environ.get("ID")
-AWS_SECRET_ACCESS_KEY = os.environ.get("KEY")
-AWS_STORAGE_BUCKET_NAME = "ssizen-net-7cmdehdrb"
-AWS_AUTO_CREATE_BUCKET = True
-AWS_DEFAULT_ACL = "public-read"
 
 if not DEBUG:
+
+    DEFAULT_FILE_STORAGE = "config.custom_storages.UploadStorage"
+    STATICFILES_STORAGE = "config.custom_storages.StaticStorage"
+    AWS_ACCESS_KEY_ID = os.environ.get("ID")
+    AWS_SECRET_ACCESS_KEY = os.environ.get("KEY")
+    AWS_STORAGE_BUCKET_NAME = "ssizen-net-7cmdehdrb"
+    AWS_AUTO_CREATE_BUCKET = True
+    AWS_DEFAULT_ACL = "public-read"
+
+    AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+    STATIC_URL = f"http://{AWS_S3_CUSTOM_DOMAIN}/static/"
+    MEDIA_URL = f"http://{AWS_S3_CUSTOM_DOMAIN}/media/"
 
     sentry_sdk.init(
         dsn=os.environ.get("SENTRY_URL"),
